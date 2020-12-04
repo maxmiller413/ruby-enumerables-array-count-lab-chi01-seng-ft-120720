632f236bab1ab998b6a2c3a03e961ae51063ebed
Max Miller <maxmiller413@gmail.com>
2020-12-04 05:17:16 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,6 @@
 def count_strings(array)
   array.count do |element|
     if element == String
-  end
   
   # Return the total number of strings in the provided array using the count enumerable
 end
