9237f0c9642486cce9453c21ff7ef639092441d7
Max Miller <maxmiller413@gmail.com>
2020-12-04 05:29:02 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,6 +3,7 @@ def count_strings(array)
     if element == String
   end 
   # Return the total number of strings in the provided array using the count enumerable
+  array
 end
 
 def count_empty_strings(array)
@@ -10,4 +11,5 @@ def count_empty_strings(array)
     if element == []
   end 
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array 
 end
